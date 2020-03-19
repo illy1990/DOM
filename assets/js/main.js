13a6e85fb6b72c12cfd73c35542c6dd1def3f600
illy1990 <jascha.hell@outlook.de>
2020-03-19 11:45:31 +0100

    Aufgabe 1 fertig

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+
+function myFunction() {
+
+    for (let index = 0; index < document.getElementsByClassName("example").length; index++) {
+        document.getElementsByClassName("example")[index].style.backgroundColor = "blue";
+    }
+}

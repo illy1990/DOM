bc2b308b9e3882f39f9937566cafe53fe7a0b06d
illy1990 <jascha.hell@outlook.de>
2020-03-19 13:19:22 +0100

    Aufgabe 3 fertig

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,3 +11,15 @@ function ClickMeOnce() {
     document.getElementById("navHome").style.backgroundColor = "pink";
     document.getElementById("navHome").innerHTML = "emoH"
 }
+
+function formular() {
+    var VorName = document.getElementById("vorname").value;
+    var NachName = document.getElementById("nachname").value;
+
+    if (document.getElementById("mySelect1").selected) { var Land = "Australia" }
+    else if (document.getElementById("mySelect2").selected) { var Land = "Canada" }
+    else { var Land = "USA" }
+    let output = "Full Name: " + VorName + " " + NachName + " Land: " + Land
+
+    console.log(output)
+}
